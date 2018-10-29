7a6d0da05db00a25397270eedc6280e72faf380a
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-29 18:55:59 +0000

    automatically backed up by learn

---
+++ app/models/song.rb
@@ -0,0 +1,14 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :released, inclusion: { in: [ true, false ] }
+  validates :release_year, presence: true, if: :released
+  validates :artist_name, presence: true
+  validate :date_cannot_be_in_the_future
+
+
+  def date_cannot_be_in_the_future
+    if release_year.present? && release_year >= Date.today
+      errors.add(:release_year, "can't be in the future")
+    end
+  end
+end
